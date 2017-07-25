451d7e52ca58333ddbd5ac5036c83148873af025
Yi Rui <yrui@sankyinc.com>
2017-07-25 14:27:19 -0400

    add new app

--- src/pages/post/post.ts
+++ src/pages/post/post.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+  selector: 'page-post',
+  templateUrl: 'post.html'
 })
-export class AboutPage {
+export class PostPage {
 
   constructor(public navCtrl: NavController) {
 
--- src/pages/save/save.ts
+++ src/pages/save/save.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: 'page-save',
+  templateUrl: 'save.html'
 })
-export class ContactPage {
+export class SavePage {
 
   constructor(public navCtrl: NavController) {
 
